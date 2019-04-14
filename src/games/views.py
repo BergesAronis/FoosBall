aa34b5878321ffbd74bffda786b7f34afe79a934
BergesIrani <iraniber@hotmail.com>
2019-04-13 20:11:01 -0400

    Update api methods

--- src/games/views.py
+++ src/games/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 
 # Create your views here.
-@api_view()
+@api_view(['POST'])
 def index(request):
     response = {}
     user = request.user
